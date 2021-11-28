410ed568082e6ad7a95ae9b8a0ca0aaaa197f98f
Philip Oetinger <phil.oetinger@hotmail.com>
2021-11-28 16:51:58 +0100

    Add basic structopt and logger

---
+++ src/arguments.rs
@@ -0,0 +1,22 @@
+//! My newest Rust program needs a destription.
+use std::path;
+
+/// The main commands and flags.
+#[derive(Debug, structopt::StructOpt)]
+#[structopt(author)]
+pub (crate) struct Arguments {
+    /// Subcommand to run.
+    #[structopt(subcommand)]
+    pub(crate) subcommand: Subcommand,
+
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    pub(crate) verbose: u8,
+}
+
+/// Main subcommands of the program.
+#[derive(Debug, structopt::StructOpt)]
+pub (crate) enum Subcommand {
+    /// Basic subcommand
+    Run,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+mod arguments;
+
+use structopt::StructOpt;
+use arguments::{Arguments, Subcommand};
+
+/// Setup the application environment.
+fn setup(verbose: u8) {
+    // Make panics log to the logger.
+    std::panic::set_hook(Box::new(|panic_info| {
+        log::error!("{}", panic_info);
+    }));
+
+    // Initialize logger.
+    pretty_env_logger::formatted_builder()
+        .filter_level(match verbose {
+            0 => log::LevelFilter::Warn,
+            1 => log::LevelFilter::Info,
+            2 => log::LevelFilter::Debug,
+            _ => log::LevelFilter::Trace,
+        })
+        .init();
+    log::trace!("Verbose logging enabled.");
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    // Capture command line arguments.
+    let Arguments { subcommand, verbose, ..} = Arguments::from_args();
+
+    // Perform setup.
+    setup(verbose);
+
+    match subcommand {
+        Subcommand::Run => {
+            log::info!("Running...");
+            Ok(())
+        },
+    }
 }
